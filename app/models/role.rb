74079cd3d31639af82eb2ddb70fcd49058fbbb41
Juan Lebrijo <juan@lebrijo.com>
2014-06-03 16:08:29 +0200

    3.5. Named scope which retrieve all roles which begins with a character

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ActiveRecord::Base
   has_many :users
   has_and_belongs_to_many :permissions
+
+  scope :all_begining_with, ->(character) { where "name like ?", "#{character}%"}
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -4,4 +4,13 @@ describe Role, type: :model do
   it { should have_db_column(:name) }
   it { should have_and_belong_to_many :permissions }
   it { should have_many :users }
+
+  describe :all_begining_with do
+    let(:admin) { create :admin }
+    let(:author) { create :role, name: "author" }
+    it "should retrieve all roles which begin with a character" do
+      Role.all_begining_with("a").should == [admin, author]
+      Role.all_begining_with("b").should be_empty
+    end
+  end
 end
